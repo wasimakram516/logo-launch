f8a52f4849c6618de447fd688146c0bb171a8ffc
Ali <ali.nagib1996@gmail.com>
2025-02-18 14:44:00 +0400

    Update IframeModal.js
    Added notWorking URLs

--- src/components/IframeModal.js
+++ src/components/IframeModal.js
@@ -10,7 +10,7 @@ const ModalComponent = ({ open, handleClose, iframeSrc }) => {
   const [key, setKey] = useState(0); // ✅ Forces re-render
   const isTouchDevice = useMediaQuery("(max-width: 1024px)"); // ✅ Detects mobile screens
 
-  const notWorking= ["http://abc@gmail.com", "","",""];
+  const notWorking= ["https://www.durag.com/en/index.htm", "https://www.advancedenergy.com/en-us/","https://www.watertechnologies.com/lp-veolia","https://www.freudenberg.com/"];
   
   useEffect(() => {
     setKey((prev) => prev + 1); // ✅ Re-renders when full-screen is toggled
