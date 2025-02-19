6eecf4457245e11da3f93b9e89454e1704b8db29
Wasim Akram <mr.saim516@gmail.com>
2025-02-19 17:37:55 +0500

    update / Changed ambient backgrouond to linear css based backeground

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
-  Container,
   Box,
-  Typography,
   IconButton,
   Table,
   TableBody,
@@ -19,7 +17,6 @@ import Image from "next/image";
 import Link from "next/link";
 import companyLogo from "@/assets/aims-logo.png";
 
-import { Shift } from "ambient-cbg";
 
 const Homepage = () => {
   const [open, setOpen] = useState(false);
@@ -61,16 +58,16 @@ const Homepage = () => {
   };
 
   return (
-    <Container
+    <Box
       sx={{
         paddingY: 4,
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+        backgroundImage: "linear-gradient(to right, #ffd89b, #19547b)"
       }}
     >
-      <Shift />
       {/* Login Button - Top Right */}
       <IconButton
         sx={{
@@ -217,7 +214,7 @@ const Homepage = () => {
         handleClose={handleClose}
         iframeSrc={iframeSrc}
       />
-    </Container>
+    </Box>
   );
 };
 
